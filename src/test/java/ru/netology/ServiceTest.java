e59f705f2a5839c9aae180c49b5e08f226144909
Владимир Сизов <7580844@mail.ru>
2022-09-30 14:35:54 +0300

    test failed

--- src/test/java/ru/netology/ServiceTest.java
+++ src/test/java/ru/netology/ServiceTest.java
@@ -2,6 +2,7 @@ package ru.netology;
 
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class ServiceTest {
     @Test
@@ -17,7 +18,7 @@ public class ServiceTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body(/* --> ваша проверка здесь <-- */)
+                .body("data", equalTo("some value"))
         ;
     }
 }
